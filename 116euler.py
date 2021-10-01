41c62085970fe7352d9d77f7320d9d838e9fbbb2
paolopoli1980 <paolopoli1980@gmail.com>
2021-10-01 20:47:04 +0200

    Add files via upload

--- 116euler.py
+++ 116euler.py
@@ -1,3 +1,20 @@
+'''
+A row of five grey square tiles is to have a number of its tiles replaced with coloured oblong tiles chosen from red (length two), green (length three), or blue (length four).
+
+If red tiles are chosen there are exactly seven ways this can be done.
+
+png116_1.png
+If green tiles are chosen there are three ways.
+
+png116_2.png
+And if blue tiles are chosen there are two ways.
+
+png116_3.png
+Assuming that colours cannot be mixed there are 7 + 3 + 2 = 12 ways of replacing the grey tiles in a row measuring five units in length.
+
+How many different ways can the grey tiles in a row measuring fifty units in length be replaced if colours cannot be mixed and at least one coloured tile must be used?
+
+'''
 cont=0
 cont2=0
 number=50
@@ -23,7 +40,7 @@ for el in lista:
 		cont=cont+1
 		n=number-lista[cont2]*cont+cont	
 		k=cont
-		print binom(n,k)
+		print (binom(n,k))
 		somma=somma+binom(n,k)
 	cont2+=1
-print somma
+print (somma)
