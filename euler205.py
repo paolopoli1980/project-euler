50d9dcc926d73263e180066b3bf910b105299b89
paolopoli1980 <paolopoli1980@gmail.com>
2021-10-01 20:50:35 +0200

    Add files via upload

--- euler205.py
+++ euler205.py
@@ -3,6 +3,12 @@
 Created on Sat Oct 01 11:49:14 2016
 
 @author: paolo
+Peter has nine four-sided (pyramidal) dice, each with faces numbered 1, 2, 3, 4.
+Colin has six six-sided (cubic) dice, each with faces numbered 1, 2, 3, 4, 5, 6.
+
+Peter and Colin roll their dice and compare totals: the highest total wins. The result is a draw if the totals are equal.
+
+What is the probability that Pyramidal Pete beats Cubic Colin? Give your answer rounded to seven decimal places in the form 0.abcdefg
 """
 import math
 max1=600000
